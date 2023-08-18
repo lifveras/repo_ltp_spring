ca545ecbf2867007ca88b9b0d0e6996c3939bb21
Professor <gustavo_veras@ifsp.edu.br>
2023-08-18 14:58:52 -0300

    Criação do Projeto Spring

---
+++ src/main/java/edu/ifsp/joguinho/joguinhoapi/JoguinhoApiApplication.java
@@ -0,0 +1,13 @@
+package edu.ifsp.joguinho.joguinhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoguinhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoguinhoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ifsp/joguinho/joguinhoapi/JoguinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifsp.joguinho.joguinhoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoguinhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
